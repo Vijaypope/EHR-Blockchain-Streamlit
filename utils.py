1fbe75abfdd3d276e9a14a97d237f33b926e0136
Vijaypope <147377662+Vijaypope@users.noreply.github.com>
2025-05-04 10:01:34 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,1 +1,28 @@
+import hashlib
+import time
 
+class Block:
+    def __init__(self, index, data, previous_hash):
+        self.index = index
+        self.timestamp = time.time()
+        self.data = data
+        self.previous_hash = previous_hash
+        self.hash = self.calculate_hash()
+
+    def calculate_hash(self):
+        return hashlib.sha256(f"{self.index}{self.timestamp}{self.data}{self.previous_hash}".encode()).hexdigest()
+
+class Blockchain:
+    def __init__(self):
+        self.chain = [self.create_genesis_block()]
+
+    def create_genesis_block(self):
+        return Block(0, "Genesis Block", "0")
+
+    def add_block(self, data):
+        last_block = self.chain[-1]
+        new_block = Block(len(self.chain), data, last_block.hash)
+        self.chain.append(new_block)
+        return new_block
+
+ehr_chain = Blockchain()
