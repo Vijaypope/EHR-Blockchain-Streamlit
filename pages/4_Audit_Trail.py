dc583072fe6f6dc1396ef27e9740c2deff87517f
Vijaypope <147377662+Vijaypope@users.noreply.github.com>
2025-05-04 10:12:02 +0530

    Update 4_Audit_Trail.py

--- pages/4_Audit_Trail.py
+++ pages/4_Audit_Trail.py
@@ -1,1 +1,17 @@
+import streamlit as st
+from utils import get_chain
 
+st.title("Blockchain Audit Trail")
+
+chain = get_chain()
+
+if not chain:
+    st.warning("No blocks yet.")
+else:
+    for block in chain:
+        st.code(f"""
+Block #{block['index']}
+Data: {block['data']}
+Hash: {block['hash']}
+Prev: {block['previous_hash']}
+        """, language="text")
